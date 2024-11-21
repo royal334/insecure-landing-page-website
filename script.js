c3b8ead8b3dc38e3332267bdc8a28ec62e54e35d
Sam-Otuonye Royal <samotuonyeroyal@gmail.com>
2024-11-21 14:44:23 +0100

    Done with project

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const navBar = document.getElementById('nav-bar');
 const  hamburger = navBar.querySelector('a img');
 const nav = document.querySelector('.nav')
 
+let toggle = false
+
 function closeUp(){
-     hamburger.src = "./images/icon-hamburger.svg" ? '/images/icon-close.svg' : "./images/icon-hamburger.svg"
+    
+     
+     if (toggle === false ){
+          hamburger.src = '/images/icon-close.svg'
+          console.log(toggle)
+     }
+     else{
+          hamburger.src = "./images/icon-hamburger.svg"
+     }
+     toggle = !toggle
      nav.classList.toggle('nav--visible');
+     
 }
 
